d476ae0aa3528c3ad3f48bf492d613942b0c5d1c
Bati <no-reply@batijs.dev>
2026-05-13 16:52:50 +0200

    scaffold Vike app with Bati

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+/// <reference types="@batijs/core/types" />
+
+import vike from "vike/plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [vike(), react(), tailwindcss()],
+});
